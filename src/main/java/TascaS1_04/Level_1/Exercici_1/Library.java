228359d114812166a7f22c0c5c2be48c28a7e959
Lois <loisgonzalezalonso@gmail.com>
2025-07-10 13:59:55 +0200

    Semana 2

--- src/main/java/TascaS1_04/Level_1/Exercici_1/Library.java
+++ src/main/java/TascaS1_04/Level_1/Exercici_1/Library.java
@@ -1,22 +1,41 @@
 package TascaS1_04.Level_1.Exercici_1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
     List<String> booksCollection;
 
     public Library() {
+        this.booksCollection = new ArrayList<>();
     }
 
 
     public void setBook(String bookName) {
-        this.booksCollection.add(bookName);
+        if (!this.booksCollection.contains(bookName)){
+            this.booksCollection.add(bookName);
+        }
+        else{
+            System.out.println("Book '" + bookName + "' not added, you already have it in your collection.");
+        }
+
     }
-    public void setBookByPosition(int position, String name){
-        this.booksCollection.add(position, name);
+    public void setBookByPosition(int position, String bookName) {
+        if (!this.booksCollection.contains(bookName)){
+            while (this.booksCollection.size() <= position) {
+                this.booksCollection.add(null); // Añade null al final para expandir la lista
+                System.out.println("Rellenando con null en el índice: " + (this.booksCollection.size() - 1));
+            }
+            this.booksCollection.set(position, bookName);
+        }
+        else{
+            System.out.println("Book '" + bookName + "' not added, you already have it in your collection.");
+        }
     }
 
+
     public String getBookByPosition(int position){
+       // System.out.println(this.booksCollection.get(position));
         return this.booksCollection.get(position);
     }
 
--- src/main/java/TascaS1_04/Level_1/Exercici_1/Main.java
+++ src/main/java/TascaS1_04/Level_1/Exercici_1/Main.java
@@ -3,5 +3,20 @@ package TascaS1_04.Level_1.Exercici_1;
 public class Main {
     public static void main(String[] args){
 
+        Library library = new Library();
+
+        library.setBookByPosition(89, "La sombra del viento");
+
+
+        System.out.println(library.getBookByPosition(89));
+        library.setBookByPosition(56, "Días de verano");
+        System.out.println(library.getBookByPosition(56));
+        library.setBookByPosition(89, "Tots el mestres");
+
+
+        System.out.println(library.getBookByPosition(89));
+
+        library.setBook("Tots el ");
+
     }
 }
